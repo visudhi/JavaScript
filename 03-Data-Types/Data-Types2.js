8f4db72d95983a58824329b41b5e87690808a618
PB Sathyanarayan <pleasantnow@gmail.com>
2023-02-18 22:56:26 +0530

    Data-Types added

---
+++ 03-Data-Types/Data-Types2.js
@@ -0,0 +1,31 @@
+/*Number DataType*/
+
+const z=8.12 // its value is 8.12
+const x=11;  // its value is 11
+
+let result=100; // its value is 100
+
+let result1 =0/0; // its value is NaN
+
+let result2= "Ten"*5; // its value is NaN
+
+
+/*String DataType*/
+
+let xName="abc";
+let xName1='abc';
+
+let x1="abcd'x";
+let x2='abcd"x';
+
+/*Literals*/
+/*Literals
+
+Literals can span 
+multiple lines and interpolate expressions to include their results.*/
+let fName="Abc";
+let lName="Def";
+console.log("Name: "+fName+" "+
+lName+"\nEmail: "+fName+"-"+lName+"@xyz.com");
+
+console.log(`fName:${fName} ${lName} Email: ${fName}_${lName}@xyz.com`); // we use backticks here , below the escape key
