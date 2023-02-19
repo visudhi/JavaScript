523600156d363352e82f105780e38189edeb0b00
PB Sathyanarayan <pleasantnow@gmail.com>
2023-02-19 10:57:32 +0530

    Data-Types5.js modified

---
+++ 03-Data-Types/Data-Types5.js
@@ -0,0 +1,18 @@
+/*Non-Primitive DataTypes*/
+//The data type is said to be non-primitive if it is a collection of multiple values. 
+
+//Syntax: 
+/*
+{ 
+  key1 : value1,  
+  key2 : value2, 
+  key3 : value3 
+};
+*/
+
+let Car={
+    name: "Santro",
+    brand: "Hyundai",
+    maps_platform:"android",
+    price:10+"lakhs"
+};
